995ddaab3717d7cc66889241fb16a52144d59358
Jeya Prakash <anthony.jeyaprakash@gmail.com>
2019-01-24 08:55:38 +0530

    express module added.

--- index.ts
+++ index.ts
@@ -1,5 +1,18 @@
 import sdk from '@stackblitz/sdk';
 import './styles.css';
+import * as express from "express";
+
+// const PORT = Number(process.env.PORT) || 8080;
+const PORT = 8080;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("🎉 Hello TypeScript! 🎉");
+});
+
+const server: object = app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
 
 // Open the RealWorld Angular repo in a new window on StackBlitz
 // https://github.com/gothinkster/angular-realworld-example-app
@@ -15,3 +28,4 @@ window['embedGithubProject'] = () => {
     { height: 320 }
   );
 }
+
